01d48cb97d8031bca4cf00c3f1a36d8081c7ae13
matsmo333 <matsumo333@icloud.com>
2024-05-30 14:58:49 +0900

    一応形にはなりましたが

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,190 +1,100 @@
-
 import React, { useState, useEffect } from "react";
-import {
-  signInWithPopup,
-  signInWithEmailAndPassword,
-  getAuth,
-  isSignInWithEmailLink,
-  signInWithEmailLink,
-  sendPasswordResetEmail,
-} from "firebase/auth";
-import { provider } from "../firebase";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
 import { useNavigate } from "react-router-dom";
-import Modal from "react-modal";
 import "./main.scss";
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
   const navigate = useNavigate();
-  const firebaseAuth = getAuth();
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isResetModalOpen, setIsResetModalOpen] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [resetEmail, setResetEmail] = useState("");
+  const [accountName, setAccountName] = useState(null);
 
-  useEffect(() => {
-    if (isSignInWithEmailLink(firebaseAuth, window.location.href)) {
-      let email = window.localStorage.getItem("emailForSignIn");
-      if (!email) {
-        email = window.prompt("Please provide your email for confirmation");
-      }
-      signInWithEmailLink(firebaseAuth, email, window.location.href)
-        .then((result) => {
-          window.localStorage.removeItem("emailForSignIn");
-          navigate("/"); // サインイン成功後にリダイレクト
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-    }
-  }, [firebaseAuth, navigate]);
+  const handleGoogleLogin = async () => {
+    try {
+      const auth = getAuth();
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      const user = result.user;
+      setIsAuth(true);
 
-  const loginInWithGoogle = () => {
-    signInWithPopup(firebaseAuth, provider)
-      .then((result) => {
-        localStorage.setItem("isAuth", true);
-        console.log(result);
+      // ユーザーの UID で members コレクションをクエリして accountname を取得
+      const membersQuery = query(
+        collection(db, "members"),
+        where("author.id", "==", user.uid)
+      );
+      const membersSnapshot = await getDocs(membersQuery);
+
+      if (!membersSnapshot.empty) {
+        const userDoc = membersSnapshot.docs[0]; // Assuming there's only one matching document
+        const userData = userDoc.data();
+        setAccountName(userData.accountname);
         navigate("/");
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+      } else {
+        console.log("Account name not found. Redirecting to registration.");
+        navigate("/member-registration");
+      }
+
+    } catch (error) {
+      console.error("Failed to sign in with Google.", error);
+    }
+  };
+
+  const handleEmailLogin = () => {
+    navigate("/emaillogin");
   };
 
   const redirectToSignupForm = () => {
     navigate("/signupform");
   };
 
-  const handleEmailSignIn = () => {
-    setIsModalOpen(true);
-  };
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        try {
+          const membersQuery = query(
+            collection(db, "members"),
+            where("author.id", "==", user.uid)
+          );
+          const membersSnapshot = await getDocs(membersQuery);
 
-  const handleSignIn = () => {
-    signInWithEmailAndPassword(firebaseAuth, email, password)
-      .then((result) => {
-        localStorage.setItem("isAuth", true);
-        console.log(result);
-        navigate("/");
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("Failed to sign in. Please check your email and password.");
-      })
-      .finally(() => {
-        setIsModalOpen(false);
-      });
-  };
+          if (!membersSnapshot.empty) {
+            const userDoc = membersSnapshot.docs[0]; // Assuming there's only one matching document
+            const userData = userDoc.data();
+            setAccountName(userData.accountname);
+          } else {
+            console.log("Account name not found. Redirecting to registration.");
+            navigate("/member");
+          }
+        } catch (error) {
+          console.error("Error fetching account name:", error);
+        }
+      }
+    });
 
-  const handlePasswordReset = (e) => {
-    e.preventDefault();
-    sendPasswordResetEmail(firebaseAuth, resetEmail)
-      .then(() => {
-        alert("Password reset email sent!");
-        setIsResetModalOpen(false);
-      })
-      .catch((error) => {
-        console.error(error);
-        alert("Error sending password reset email");
-      });
-  };
+    return unsubscribe;
+  }, [navigate]);
 
   return (
     <div className="container">
       <div className="content">
-        <p>Googleでログイン</p>
-        <button className="login-button" onClick={loginInWithGoogle}>
+        <p>Googleアカウントでログイン</p>
+        <button className="button login-button" onClick={handleGoogleLogin}>
           Googleでログイン
         </button>
       </div>
       <div className="content">
         <p>メールアドレスでログイン</p>
-        <button className="login-button" onClick={handleEmailSignIn}>
+        <button className="button login-button" onClick={handleEmailLogin}>
           メールアドレスでログイン
         </button>
       </div>
       <div className="content">
         <p>新たに登録を実施</p>
-        <button onClick={redirectToSignupForm}>新規登録</button>
+        <button className="button login-button" onClick={redirectToSignupForm}>
+          新規登録
+        </button>
       </div>
-
-      <Modal
-        isOpen={isModalOpen}
-        onRequestClose={() => setIsModalOpen(false)}
-        contentLabel="Email Sign-In Modal"
-        className="modal"
-        overlayClassName="overlay"
-      >
-        <button className="close-button" onClick={() => setIsModalOpen(false)}>
-          ｘ
-        </button>
-        <h2>メールアドレスでログイン</h2>
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSignIn();
-          }}
-        >
-          <div className="modal-content">
-            <label>Email:</label>
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              className="modal-input"
-              required
-            />
-            <label>Password:</label>
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="modal-input"
-              required
-            />
-          </div>
-          <button type="submit" className="modal-button">
-            ログイン
-          </button>
-          <button
-            type="button"
-            className="modal-button2"
-            onClick={() => setIsResetModalOpen(true)}
-          >
-            パスワードを忘れた場合
-          </button>
-        </form>
-      </Modal>
-
-      <Modal
-        isOpen={isResetModalOpen}
-        onRequestClose={() => setIsResetModalOpen(false)}
-        contentLabel="Password Reset Modal"
-        className="modal"
-        overlayClassName="overlay"
-      >
-        <button
-          className="close-button"
-          onClick={() => setIsResetModalOpen(false)}
-        >
-          ｘ
-        </button>
-        <h2>パスワードをリセット</h2>
-        <form onSubmit={handlePasswordReset}>
-          <div className="modal-content">
-            <label>Email:</label>
-            <input
-              type="email"
-              value={resetEmail}
-              onChange={(e) => setResetEmail(e.target.value)}
-              className="modal-input"
-              required
-            />
-          </div>
-          <button type="submit" className="modal-button">
-            パスワードリセット
-          </button>
-        </form>
-      </Modal>
     </div>
   );
 };
